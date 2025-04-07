29761e960b2de704ee7b5e70db92c152183a6341
keerthana9944 <herald.keerthana@gmail.com>
2025-04-07 11:09:37 +0530

    Completed the assignment

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const bcrypt = require("bcrypt");
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
 const { resolve } = require('path');
+const User = require('./model/user');
+
 
 const app = express();
+app.use(express.json());
 const port = 3010;
 
 app.use(express.static('static'));
@@ -12,4 +19,33 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
+});
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(()=> console.log("Connected Successfully"))
+  .catch((err) => console.error("Connection failed", err))
+
+app.post('/register', async(req, res) => {
+  const {username, email, password} = req.body;
+
+  if(!username || !email || !password){
+    return res.status(400).json({message: "Field cannot be empty"});
+  }
+
+  try{
+    const hashedpassword = await bcrypt.hash(password, 10);
+
+    const newUser = new User({
+      username, email, password: hashedpassword,
+    });
+
+    await newUser.save();
+
+    res.status(201).json({message: "Registered successfully"});
+  }
+
+  catch(error){
+    console.error("Registration error", error);
+    res.status(500).json({message: "Registration failed"});
+  }
 });
